39ec3772a81effdfa246c45dbd3dbfed80a600b8
Sarah Hunt <shunt11@live.co.uk>
2025-02-21 08:05:12 +0000

    FIx error

--- tech-trans/next.config.js
+++ tech-trans/next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "v0.blob.com",
+        port: "",
+        pathname: "/**",
+      },
+    ],
+  },
 }
 
 module.exports = nextConfig
